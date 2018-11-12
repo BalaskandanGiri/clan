2e3e03f57ef80ae7b0f2ebccf02d5e5e473fc23a
BalaskandanGiri <balaskandangiri@gmail.com>
2018-11-12 08:37:29 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import './App.css';
+
+// import logo from './logo.svg';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
